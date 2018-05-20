2e6b926ee17a2d16cf2ed6f6382bf382ce0baaf5
huangshuihui <shuihui_huang@163.com>
2018-05-20 16:08:09 +0800

    修改字符编码为UTF-8

--- src/executionEngine/DriverScript.java
+++ src/executionEngine/DriverScript.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-/*
- * 模拟百度登录的过程
- * */
+
 
 public class DriverScript {
 	
@@ -25,17 +23,17 @@ public class DriverScript {
 		driver.get("https://www.baidu.com/");
 		Thread.sleep(2000);
 		
-		//点击登录链接
-		driver.findElement(By.xpath(".//*[@id='u1']/a[text()='登录']")).click();
 		
-		//点击用户名密码登录
+		driver.findElement(By.xpath(".//*[@id='u1']/a[text()='锟斤拷录']")).click();
+		
+		
 		driver.findElement(By.id("TANGRAM__PSP_10__footerULoginBtn")).click();
 		
-		//输入账号和密码登录
+		
 		driver.findElement(By.id("TANGRAM__PSP_10__userName")).sendKeys("13826515408");
 		driver.findElement(By.id("TANGRAM__PSP_10__password")).sendKeys("hsh13141365");
 		
-		//点击登录
+		
 		driver.findElement(By.id("TANGRAM__PSP_10__submit")).click();
 		
 	}
